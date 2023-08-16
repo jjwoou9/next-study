3f90e11d19d91072b27e6e3d80c4ebf2b40ceded
정지우 <cheltus11@gmailcom>
2023-08-16 14:34:13 +0900

    feedback, firebase 추가

--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -1,32 +1,48 @@
-import React, { Fragment } from 'react'
-import Header from '../components/common/Header'
-import styles from '../styles/header.module.scss'
-import Link from 'next/link';
-import { AiOutlineShareAlt } from 'react-icons/ai';
-import { VscFeedback } from 'react-icons/vsc';
-import MapSection from '@/components/home/MapSection';
+import { Fragment } from 'react';
+import { GetServerSideProps, NextPage } from 'next';
+import { NextSeo } from 'next-seo';
+import Header from '../components/common/Header';
+import FeedbackSection from '../components/feedback/FeedbackSection';
+import type { Feedback } from '../types/feedback';
+import { getFeedbackListFromFirestore } from '../firebase/feedback';
 
-export default function feedback() {
+interface Props {
+  initialFeedbackList: Feedback[];
+}
+
+export const FeedbackPage: NextPage<Props> = ({ initialFeedbackList }) => {
   return (
     <Fragment>
-         <Header rightElements={[
-          <button onClick={() => {
-            alert("복사");
-          }}
-          className={styles.box}
-          style={{marginRight: 8}}
-          key='button'>
-             <AiOutlineShareAlt size={20} color="#444444" />
-          </button>,
-          <Link href="/feedback" className={styles.box} key="link">
-            <VscFeedback size={20} />
-          </Link>
+      <NextSeo
+        title="피드백"
+        description="매장 지도 서비스에 대한 피드백을 받습니다."
+        canonical="https://inflearn-nextjs.vercel.app/feedback"
+        openGraph={{
+          url: 'https://inflearn-nextjs.vercel.app/feedback',
+        }}
+      />
+      <Header />
 
-         ]} />
-         
-        <main style={{width: '100%', height: '100%'}}>
-          <MapSection/>
-        </main>
+      <main
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+          touchAction: 'pinch-zoom',
+        }}
+      >
+        <FeedbackSection initialFeedbackList={initialFeedbackList} />
+      </main>
     </Fragment>
   );
-}
+};
+export default FeedbackPage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      initialFeedbackList: await getFeedbackListFromFirestore(),
+    },
+  };
+};
